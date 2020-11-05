69ff2744e5bd88e782e4a96bb523a065e7171bba
ISlack7 <ian.slack@patelhs.org>
2020-11-05 13:43:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    //Declaring the seat numbers and the array
+    int [] seatNumber 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15;
+   //Declaring the string array of student names 1-15
+    String[] names = new String[15];
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String name = scan.next();
+
+
+  }
+}
