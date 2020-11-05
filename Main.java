c3d1c40559f4c2876a86472da332fa11dd9597b8
ISlack7 <ian.slack@patelhs.org>
2020-11-05 14:24:24 +0000

    Added an int array

--- Main.java
+++ Main.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     //Declaring the seat numbers and the array
-    int [] seatNumber 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15;
+    int [] seatNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
    //Declaring the string array of student names 1-15
     String[] names = new String[15];
     Scanner scan = new Scanner(System.in);
-    System.out.println("What is your name?");
-    String name = scan.next();
-
+    
+    
+    for (int i = 0; i < seatNumber.length; i++){
+      System.out.println("What is your name?");
+      names[i] = scan.next();
+    }
+    int i = 0;
+    while (i < 15){
+      System.out.println(names[i] + " Sits here " + seatNumber[i]);
+     i++;
+    }
 
   }
 }
